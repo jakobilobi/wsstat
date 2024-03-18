03320d39df2492610fd89848e122485c3fc156fc
Jakob Andersson <erikjakobandersson@gmail.com>
2024-03-18 20:33:05 +0100

    Make initial implementation print prettier

--- main.go
+++ main.go
@@ -14,20 +14,18 @@ func main() {
 	args := flag.Args()
 	if len(args) == 1 {
 		// Print the first argument
-		fmt.Printf("Input arg: %s", args[0])
+		fmt.Printf("Input arg: %s\n\n", args[0])
 	} else {
 		fmt.Println("No or too many input arg")
 		return
 	}
 
-	result, response, err := wsstat.MeasureLatency(args[0], "testing")
+	result, _, err := wsstat.MeasureLatency(args[0], "testing")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// Print the latency
-	fmt.Printf("Latency: %v", result)
-	// Print the response
-	fmt.Printf("Response: %v", response)
+	fmt.Printf("Results: \n%+v", result)
 }
